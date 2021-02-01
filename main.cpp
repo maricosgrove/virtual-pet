0dcd8f250bb6b92a96ceeffea144dd158e7f865b
Mari <72470827+maricosgrove@users.noreply.github.com>
2021-01-31 23:58:44 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
-//big ole test
+//executes the alpha menu
 
-void main() {
+void display();
+void petMenu();
 
+int main() {
+display();
+}
+
+void display() {
+//"Hello! Welcome to your virtual pet house."
+//"How can I help you?"
+
+//View pet
+petMenu);
+
+//View inventory
+
+//View shop
+
+//View gameroom
+}
+
+void petMenu() {
+//Mood blurb
+//Stat display
+
+//pet menu
 }
