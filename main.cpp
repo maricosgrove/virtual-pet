d886c981f2f238a2c791c2e2426bc39db60fba58
Mari <72470827+maricosgrove@users.noreply.github.com>
2021-02-01 21:41:34 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,121 @@
 //executes the alpha menu
+#include "Pet.h"
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+using namespace std;
 
-void display();
+#define PLACEHOLDER_TEXT cout << "Item not finished yet"
+
+Pet* currentPet;
+
 void petMenu();
+void executeMainAction(char choice);
+void load();
 
 int main() {
-display();
+	load();
+	cout << "\nHello! Welcome to your virtual pet house."
+		<< "\nHow can I help you?\n";
+	char choice = '1';
+	do {
+		cout << "\np. Go to Pet"
+			<< "\ni. Go to Inventory"
+			<< "\ns. Go to Shop"
+			<< "\ng. Go to Game Room"
+			<< "\nq. Save and Quit\n";
+		cin >> choice;
+		cin.ignore();
+		executeMainAction(choice);
+	} while (choice != 'q');
+	return 0;
 }
 
-void display() {
-//"Hello! Welcome to your virtual pet house."
-//"How can I help you?"
-
-//View pet
-petMenu);
-
-//View inventory
-
-//View shop
-
-//View gameroom
+void petMenu();
+void inventoryMenu();
+void shopMenu();
+void gameMenu();
+void saveQuit();
+void executeMainAction(char choice) {
+	switch (choice) {
+	case 'p': petMenu(); break;
+	case 'i': inventoryMenu(); break;
+	case 's': shopMenu(); break;
+	case 'g': gameMenu(); break;
+	case 'q': saveQuit(); break;
+	}
 }
 
+void executePetAction(char choice);
 void petMenu() {
-//Mood blurb
-//Stat display
+	PLACEHOLDER_TEXT;
 
-//pet menu
+	if (!currentPet) {
+		cout << "\nWhoops, there's no pet here! Let's adopt a new pet."
+			<< "\nWhat's your pet's name?";
+		string name_input;
+		cin >> name_input;
+		cin.ignore();
+		currentPet = new Pet(name_input);
+	}
+	
+	currentPet->displayMood();
+	//Stat display
+	currentPet->displayStats();
+
+	char choice = '1';
+	do {
+		cout << "\nf. Feed"
+			<< "\nt. Give toy"
+			<< "\nc. Clean pet"
+			<< "\nn. Nap time"
+			<< "\ng. Play a game"
+			<< "\nd. Display stats again"
+			<< "\nr. Return to last menu\n";
+		cin >> choice;
+		cin.ignore();
+		executePetAction(choice);
+	} while (choice != 'r');
+}
+
+void executePetAction(char choice) {
+	//TODO: not how I want sleep to work
+
+	switch (choice) {
+	case 'f': inventoryMenu(); break;
+	case 't': inventoryMenu(); break;
+	case 'c': cout << "\nWashie washie\n"; currentPet->changeHygiene(50); break;
+	case 'n': PLACEHOLDER_TEXT; currentPet->changeSleep(100); break;
+	case 'g': gameMenu(); break;
+	case 'd': currentPet->displayStats();
+	case 'r': break;
+	}
+
+	PLACEHOLDER_TEXT;
+}
+
+void inventoryMenu() {
+	//TODO: Needs to allow using items
+	//TODO: Implement 
+	PLACEHOLDER_TEXT;
+}
+
+void shopMenu() {
+	//TODO: Implement
+	PLACEHOLDER_TEXT;
+}
+
+void gameMenu() {
+	//TODO: Implement
+	PLACEHOLDER_TEXT;
+}
+
+void load() {
+	//TODO: implement
+	PLACEHOLDER_TEXT;
+}
+
+void saveQuit() {
+	//TODO: implement;
+	PLACEHOLDER_TEXT;
 }
