847725f9a9c70a0f56c1da425bcea682f29c6022
Mari <72470827+maricosgrove@users.noreply.github.com>
2021-02-01 21:42:19 -0800

    Update Pet.h

--- Pet.h
+++ Pet.h
@@ -1,16 +1,73 @@
+#include <string>
+#include <iostream>
+using namespace std;
+//TODO: Needs decay over time
 
 class Pet {
-//Pet has needs, out of 100
-Health
-Hunger
-Hygiene
-Happiness
-Sleep
-//Needs decay over time
 
-//Constructor
+private:
+	int health, hunger, happiness, hygiene, sleep;
+	string name;
+	bool sleeping;
 
-//Gets
+public:
+	Pet(string name_input) {
+		health = 100;
+		hunger = 100;
+		happiness = 100;
+		hygiene = 100;
+		sleep = 100;
+		name = name_input;
+		sleeping = false;
+	}
 
-//Sets
-} 
+	int getHealth() {
+		return health;
+	}
+	int getHunger() {
+		return hunger;
+	}
+	int getHappiness() {
+		return happiness;
+	}
+	int getHygiene() {
+		return hygiene;
+	}
+	int getSleep() {
+		return sleep;
+	}
+
+	void changeHealth(int change) {
+		health = health + change;
+		if (health > 100) health = 100;
+	}
+	void changeHunger(int change) {
+		hunger = hunger + change;
+		if (hunger > 100) hunger = 100;
+	}
+	void changeHappiness(int change) {
+		happiness = happiness + change;
+		if (happiness > 100) happiness = 100;
+	}
+	void changeHygiene(int change) {
+		hygiene = hygiene + change;
+		if (hygiene > 100) hygiene = 100;
+	}
+	void changeSleep(int change) {
+		sleep = sleep + change;
+		if (sleep > 100) sleep = 100;
+	}
+
+	void displayMood() {
+		//TODO: changes message based on stats
+		cout << "\n" << name << " stares back.";
+	}
+
+	void displayStats() {
+		cout << "\nHealth: " << health << "%"
+			<< "\nHunger: " << hunger << "%"
+			<< "\nHappiness: " << happiness << "%"
+			<< "\nHygiene: " << hygiene << "%"
+			<< "\nSleep: " << sleep << "%";
+	}
+};
